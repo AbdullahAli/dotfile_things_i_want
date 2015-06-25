e9f79939c97dc6eb64c741db4161bca9c4a85879
Abdullah Ali <abdullah-ali@hotmail.co.uk>
2015-06-25 19:40:24 +0100

    some cleaning

--- dotfile/new_dotfiles_github/install.sh
+++ dotfile/new_dotfiles_github/install.sh
@@ -1,8 +1,19 @@
 echo 'You might need to change your default shell to zsh: `chsh -s /bin/zsh` (or `sudo vim /etc/passwd`)'
 
-dir="$HOME/Developer/paulmillr"
-mkdir -p $dir
-cd $dir
 git clone --recursive git://github.com/paulmillr/dotfiles.git
-cd dotfiles
-sudo bash symlink-dotfiles.sh
+
+
+
+ln -sf "/Applications/Sublime Text 2.app/Contents/SharedSupport/bin/subl" /usr/local/bin/subl
+
+mkdir -p ~/sublime
+cp -R ~/Desktop/dotfile_things_i_want/dotfile/new_dotfiles_github/sublime/User ~/sublime/
+
+ln -sf ~/sublime/User /Users/abdullahali/Library/Application\ Support/Sublime\ Text\ 2/Packages/User/
+
+
+git config --global user.name "Abdullah Ali"
+git config --global user.email "abdullah-ali@hotmail.co.uk"
+
+
+cp ~/Desktop/dotfile_things_i_want/dotfile/new_dotfiles_github/zsh/.zshrc ~/
